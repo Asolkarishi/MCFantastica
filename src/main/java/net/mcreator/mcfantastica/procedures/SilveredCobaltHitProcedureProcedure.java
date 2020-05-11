e67cce6010219c064ceab79202084c1d6602a6f8
Shorty123 <cm82380@djusdstudents.org>
2020-05-11 12:17:10 -0700

    Reworked silvered cobalt blade, then realized it needed to be a tool, so I made it a tool, it spawns snow and gives a slowness effect to mobs it hits

---
+++ src/main/java/net/mcreator/mcfantastica/procedures/SilveredCobaltHitProcedureProcedure.java
@@ -0,0 +1,60 @@
+package net.mcreator.mcfantastica.procedures;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.mcfantastica.MCFantasticaElements;
+
+@MCFantasticaElements.ModElement.Tag
+public class SilveredCobaltHitProcedureProcedure extends MCFantasticaElements.ModElement {
+	public SilveredCobaltHitProcedureProcedure(MCFantasticaElements instance) {
+		super(instance, 74);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			System.err.println("Failed to load dependency entity for procedure SilveredCobaltHitProcedure!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			System.err.println("Failed to load dependency x for procedure SilveredCobaltHitProcedure!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			System.err.println("Failed to load dependency y for procedure SilveredCobaltHitProcedure!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			System.err.println("Failed to load dependency z for procedure SilveredCobaltHitProcedure!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			System.err.println("Failed to load dependency world for procedure SilveredCobaltHitProcedure!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		int x = (int) dependencies.get("x");
+		int y = (int) dependencies.get("y");
+		int z = (int) dependencies.get("z");
+		World world = (World) dependencies.get("world");
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SLOWNESS, (int) 15, (int) 3));
+		world.setBlockState(new BlockPos((int) (x + 1), (int) y, (int) z), Blocks.SNOW.getDefaultState(), 3);
+		world.setBlockState(new BlockPos((int) x, (int) y, (int) (z + 1)), Blocks.SNOW.getDefaultState(), 3);
+		world.setBlockState(new BlockPos((int) (x - 1), (int) y, (int) z), Blocks.SNOW.getDefaultState(), 3);
+		world.setBlockState(new BlockPos((int) x, (int) y, (int) (z - 1)), Blocks.SNOW.getDefaultState(), 3);
+		world.setBlockState(new BlockPos((int) (x + 2), (int) y, (int) z), Blocks.SNOW.getDefaultState(), 3);
+		world.setBlockState(new BlockPos((int) x, (int) y, (int) (z + 2)), Blocks.SNOW.getDefaultState(), 3);
+		world.setBlockState(new BlockPos((int) (x - 2), (int) y, (int) z), Blocks.SNOW.getDefaultState(), 3);
+		world.setBlockState(new BlockPos((int) x, (int) y, (int) (z - 2)), Blocks.SNOW.getDefaultState(), 3);
+		world.setBlockState(new BlockPos((int) (x + 1), (int) y, (int) (z + 1)), Blocks.SNOW.getDefaultState(), 3);
+		world.setBlockState(new BlockPos((int) (x - 1), (int) y, (int) (z + 1)), Blocks.SNOW.getDefaultState(), 3);
+		world.setBlockState(new BlockPos((int) (x + 1), (int) y, (int) (z - 1)), Blocks.SNOW.getDefaultState(), 3);
+		world.setBlockState(new BlockPos((int) (x - 1), (int) y, (int) (z - 1)), Blocks.SNOW.getDefaultState(), 3);
+	}
+}
