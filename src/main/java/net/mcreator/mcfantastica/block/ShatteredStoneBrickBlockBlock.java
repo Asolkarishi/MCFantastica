5bcf8631f4307f491cd53567ca3bcf1977be3576
Asolkarishi <asolkarishi@gmail.com>
2020-05-20 18:00:28 -0700

    qawsedrftgyhujolp;['

--- src/main/java/net/mcreator/mcfantastica/block/ShatteredStoneBrickBlockBlock.java
+++ src/main/java/net/mcreator/mcfantastica/block/ShatteredStoneBrickBlockBlock.java
@@ -1,15 +1,30 @@
 
 package net.mcreator.mcfantastica.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.mcfantastica.MCFantasticaElements;
+
+import java.util.List;
+import java.util.Collections;
+
 @MCFantasticaElements.ModElement.Tag
 public class ShatteredStoneBrickBlockBlock extends MCFantasticaElements.ModElement {
-
 	@ObjectHolder("mcfantastica:shatteredstonebrickblock")
 	public static final Block block = null;
-
 	public ShatteredStoneBrickBlockBlock(MCFantasticaElements instance) {
 		super(instance, 82);
-
 	}
 
 	@Override
@@ -18,15 +33,10 @@ public class ShatteredStoneBrickBlockBlock extends MCFantasticaElements.ModEleme
 		elements.items
 				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
 	}
-
 	public static class CustomBlock extends Block {
-
 		public CustomBlock() {
-			super(
-
-					Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(2f, 10f).lightValue(0).harvestLevel(1)
-							.harvestTool(ToolType.PICKAXE));
-
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(2f, 10f).lightValue(0).harvestLevel(1)
+					.harvestTool(ToolType.PICKAXE));
 			setRegistryName("shatteredstonebrickblock");
 		}
 
@@ -37,7 +47,5 @@ public class ShatteredStoneBrickBlockBlock extends MCFantasticaElements.ModEleme
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
-
 	}
-
 }
